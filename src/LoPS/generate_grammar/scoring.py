f3b43351f4a3472c0e5cbfd82e51fd7534dd05ca
wywqzzh <767746811@qq.com>
2026-05-04 20:25:38 +0800

    feat(02-02): implement state condition link learning

--- src/LoPS/generate_grammar/scoring.py
+++ src/LoPS/generate_grammar/scoring.py
@@ -62,3 +62,50 @@ def bd_score(
         axis=0,
     )
     return score, posterior
+
+
+def learn_state_condition_links(
+    data: np.ndarray,
+    nstates: np.ndarray,
+    block_message: dict[int, list[int]],
+    casual_num: int,
+    block_num: int,
+    effect_num: int,
+    alpha: float,
+    conditions: list[list[int]],
+) -> tuple[np.ndarray, list, list, list]:
+    var_num = data.shape[0]
+    bestparents = [[] for _ in range(var_num)]
+    bestparameters = [[] for _ in range(var_num)]
+    bestscores = [[] for _ in range(var_num)]
+    var_casual = list(range(block_num))
+    var_effect = list(range(var_num - effect_num, var_num))
+    learned_adjacency = np.zeros((block_num + effect_num, block_num + effect_num))
+
+    for v in var_effect:
+        for parent_block in var_casual:
+            condition = conditions[parent_block]
+            condition = [block_message[index] for index in condition]
+            condition = sum(condition, [])
+            condition_alpha = alpha / (np.prod(nstates[v]) * np.prod(nstates[condition]))
+            bd1, _ = bd_score(
+                data[v, :],
+                data[condition, :],
+                nstates[v],
+                nstates[condition],
+                condition_alpha,
+            )
+
+            parent_variables = condition + block_message[parent_block]
+            parent_alpha = alpha / (np.prod(nstates[v]) * np.prod(nstates[parent_variables]))
+            bd2, _ = bd_score(
+                data[v, :],
+                data[parent_variables, :],
+                nstates[v],
+                nstates[parent_variables],
+                parent_alpha,
+            )
+            if bd1 / bd2 > 1:
+                learned_adjacency[parent_block, v - (casual_num - block_num)] = 1
+
+    return learned_adjacency, bestparameters, bestparents, bestscores
